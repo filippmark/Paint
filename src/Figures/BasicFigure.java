6b442a9fca033db1375eadcd8de96e23f0d5a4e5
filipp <lenovofm12@outlook.com>
2019-03-09 14:46:30 +0300

    class BasicFigure changed

--- src/Figures/BasicFigure.java
+++ src/Figures/BasicFigure.java
@@ -3,5 +3,17 @@ package Figures;
 import java.awt.*;
 
 public class BasicFigure {
+    private Color color;
 
+    public void BasicFigure(){
+        this.color = Color.RED;
+    }
+
+    public Color getColor(){
+        return this.color;
+    }
+
+    public void setColor(Color color){
+        this.color = color;
+    }
 }
